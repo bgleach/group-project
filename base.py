ed6dd17515535e7965885c4774c4f084417067c8
Haifeng Wang <haifeng@udel.edu>
2017-12-05 21:48:13 -0500

    new file

--- base.py
+++ base.py
@@ -1,9 +1,31 @@
-
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask, session, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess secure key'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+class Director(db.Model):
+    __tablename__ = 'directors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    age = db.Column(db.Integer)
+    gender=db.Column(db.String(64))
+    films = db.relationship('Film', backref='director', cascade="delete")
 
 
+class Film(db.Model):
+    __tablename__ = 'films'
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(256))
+    year = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    film_id = db.Column(db.Integer, db.ForeignKey('directors.id'))
 
 @app.route('/')
 def index():
@@ -12,6 +34,16 @@ def index():
 @app.route('/members')
 def members():
     return render_template('about-members.html')
+
+@app.route('/directors')
+def show_all_directors():
+    directors = Director.query.all()
+    return render_template('director-all.html', directors=directors)
+
+@app.route('/films')
+def show_all_films():
+    films = Film.query.all()
+    return render_template('film-all.html', films=films)
 
 
 if __name__ == '__main__':
--- manage.py
+++ manage.py
@@ -1,9 +1,26 @@
 from flask_script import Manager
-from base import app
+from base import app, db, Director, Film
 
 
 manager = Manager(app)
 
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    director1 = Director(name='Ang Lee', age=63, gender='Male')
+    director2 = Director(name='Christopher Nolan', age=47, gender='Male')
+    director3 = Director(name='Kathryn Bigelow', age=66, gender='Female')
+    film1 = Film(name='Dunkirk', year=2017, description="In May 1940, Germany advanced into France, trapping Allied troops on the beaches of Dunkirk. Under air and ground cover from British and French forces, troops were slowly and methodically evacuated from the beach using every serviceable naval and civilian vessel that could be found. At the end of this heroic mission, 330,000 French, British, Belgian and Dutch soldiers were safely evacuated.", director=director1)
+    film2 = Film(name="Harry Potter and the Philosopher's Stone", year=2001, description="Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child to become a student at Hogwarts, an English boarding school for wizards. There, he meets several friends who become his closest allies and help him discover the truth about his parents' mysterious deaths.")
+    film3 = Film(name="The Shawshank Redemption", year=1994, description="Andy Dufresne (Tim Robbins) is sentenced to two consecutive life terms in prison for the murders of his wife and her lover and is sentenced to a tough prison. However, only Andy knows he didn't commit the crimes. While there, he forms a friendship with Red (Morgan Freeman), experiences brutality of prison life, adapts, helps the warden, etc., all in 19 years.")
+    db.session.add(director1)
+    db.session.add(director2)
+    db.session.add(director3)
+    db.session.add(film1)
+    db.session.add(film2)
+    db.session.add(film3)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
