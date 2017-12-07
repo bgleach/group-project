eab5cb915523858e1725163d9dd00b745af4cdb0
Haifeng Wang <haifeng@udel.edu>
2017-12-06 23:25:11 -0500

    new file added

--- base.py
+++ base.py
@@ -40,10 +40,46 @@ def show_all_directors():
     directors = Director.query.all()
     return render_template('director-all.html', directors=directors)
 
+@app.route('/director/add', methods=['GET', 'POST'])
+def add_directors():
+    if request.method == 'GET':
+        return render_template('director-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        age = request.form['age']
+        gender = request.form['gender']
+
+        # insert the data into the database
+        director = Director(name=name, age=age, gender=gender)
+        db.session.add(director)
+        db.session.commit()
+        return redirect(url_for('show_all_directors'))
+
 @app.route('/films')
 def show_all_films():
     films = Film.query.all()
     return render_template('film-all.html', films=films)
+
+@app.route('/film/add', methods=['GET', 'POST'])
+def add_films():
+    if request.method == 'GET':
+        directors = Director.query.all()
+        return render_template('film-add.html', directors=directors)
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        year = request.form['year']
+        description = request.form['description']
+        director_name = request.form['director']
+        director = Director.query.filter_by(name=director_name).first()
+        # insert the data into the database
+        film = Film(name=name, year=year, description=description, director=director)
+        db.session.add(film)
+        db.session.commit()
+        return redirect(url_for('show_all_films'))
+
+
 
 
 if __name__ == '__main__':
